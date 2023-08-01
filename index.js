4f5aced35db14e8c1c501a83e310a4c31edc60f3
MEVINDANI <mevindani14@gmail.com>
2023-08-01 20:08:06 +0530

    payment ist

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/api/users', userRoute)
 app.use('/api/products', productRoute)
 app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
-app.use('/api/checkout', stripeRoute)
+app.use('/api/stripe', stripeRoute)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("express app is running on 5000")
--- routes/stripe.js
+++ routes/stripe.js
@@ -7,6 +7,35 @@ const stripe = require('stripe')(process.env.STRIPE_KEY);
 //     apiVersion: "2022-08-01",
 // })
 
+// chooloo
+router.post('/create-checkout-session', async (req, res) => {
+    console.log(req.body.cartItems)
+    const line_items = req.body.cartItems.map((item) => {
+        return {
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: item.title,
+                    images: [item.img],
+                    description: item.desc,
+                    metadata: {
+                        id: item.id
+                    }
+                },
+                unit_amount: item.price * 100,
+            },
+            quantity: item.quantity,
+        }
+    })
+    const session = await stripe.checkout.sessions.create({
+        line_items,
+        mode: 'payment',
+        success_url: `${process.env.CLIENT_URL}/checkout_success`,
+        cancel_url: `${process.env.CLIENT_URL}/cart`,
+    })
+    res.send({ url: session.url })
+})
+
 router.get('/config', (req, res) => {
     res.status(200).json({
         publishableKey: process.env.STRIPE_PKEY,
